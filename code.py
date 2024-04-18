fc0557b855a018d41ccd8c13ba32de2aedfacb4c
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2024-04-18 10:38:31 +0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,33 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+# Read the video from specified path
+cam = cv2.VideoCapture("live_2.mp4")
+
+try:
+	# creating a folder named data
+	if not os.path.exists('Data_For_Train'):
+		os.makedirs('Data_For_Train')
+# if not created then raise error
+except OSError:
+	print ('Error: Creating directory of data')
+# frame
+currentframe = 0
+while(True):
+	# reading from frame
+	ret,frame = cam.read()
+	if ret:
+		# if video is still left continue creating images
+		name = '.Data_For_Train' + str(currentframe) + '.jpg'
+		print ('Creating...' + name)
+		# writing the extracted images
+		cv2.imwrite(name, frame)
+		# increasing counter so that it will
+		# show how many frames are created
+		currentframe += 1
+	else:
+		break
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
